b45afd9fd81557c1dacbdbaa87e2cef381a049a8
logolin <denghuoguangyin@163.com>
2017-03-15 23:20:54 +0800

    判断用户登录密码是否正确

--- src/main/java/com/li/controller/AppController.java
+++ src/main/java/com/li/controller/AppController.java
@@ -1,13 +1,20 @@
 package com.li.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.li.domain.User;
+import com.li.repository.UserRepository;
 
 @Controller
 @RequestMapping("/")
 public class AppController {
 
+	@Autowired
+	private UserRepository userRepository;
+	
 	/**
 	 * 重定向到登录页面
 	 * @return
@@ -39,7 +46,18 @@ public class AppController {
 		
 		System.out.println("用户名：" + userName);
 		System.out.println("密码：" + password);
-		return "redirect:login";
+		//根据用户名查找用户
+		User user = this.userRepository.findByUserName(userName);
+		//判断密码是否正确
+		if (null != user && user.getPassword().equals(password)) {
+			//密码正确，跳转回注册页面
+			return "redirect:register";
+		} else {
+			//密码或者账号不对，跳转回登录页
+			return "redirect:login";
+		}
+		
+		
 	}
 	
 	/**
